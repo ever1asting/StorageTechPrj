69f6b42a340f5cac1aaa70245883bea5fe00363a
ywn14 <312369144@qq.com>
2017-05-24 17:01:24 +0800

    a simplified but work version

--- src/main.c
+++ src/main.c
@@ -1,222 +1,148 @@
-/*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-
-  Minor modifications and note by Andy Sayler (2012) <www.andysayler.com>
-
-  Source: fuse-2.8.7.tar.gz examples directory
-  http://sourceforge.net/projects/fuse/files/fuse-2.X/
-
-  This program can be distributed under the terms of the GNU GPL.
-  See the file COPYING.
-
-  gcc -Wall `pkg-config fuse --cflags` fusexmp.c -o fusexmp `pkg-config fuse --libs`
-
-  Note: This implementation is largely stateless and does not maintain
-        open file handels between open and release calls (fi->fh).
-        Instead, files are opened and closed as necessary inside read(), write(),
-        etc calls. As such, the functions that rely on maintaining file handles are
-        not implmented (fgetattr(), etc). Those seeking a more efficient and
-        more complete implementation may wish to add fi->fh support to minimize
-        open() and close() calls and support fh dependent functions.
-
-*/
-
-#define FUSE_USE_VERSION 28
-#define HAVE_SETXATTR
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef linux
-/* For pread()/pwrite() */
-#define _XOPEN_SOURCE 500
-#endif
+#define FUSE_USE_VERSION 26
 
 #include <fuse.h>
-#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include <errno.h>
-#include <sys/time.h>
-#ifdef HAVE_SETXATTR
-#include <sys/xattr.h>
-#endif
+#include <stdio.h>
 
-char** pathArr;
-int cnt;
+// static const char *filepath = "/file";
+// static const char *filename = "file";
+// static const char *filecontent = "I'm the content of the only file available there\n";
+char bufBase[200] = "/home/ywn/StoragePrj/src/buffuse/";
 
-char mntPath[200] = "/home/ywn/StoragePrj/src/mntfuse";
-char bufPath[200] = "/home/ywn/StoragePrj/src/buffuse";
-
-static int xmp_getattr(const char *path, struct stat *stbuf)
+struct file
 {
-	int res;
+	char name[256];
+	char filePath[1000];
+	char bufPath[1000];
+	int size;
+	int isDownload;
+	struct file* next;
+};
 
-	if (strstr(path, mntPath) != path)// && strstr(path, bufPath) != path)
-		return 0;
-
-    printf("--------getattr, %s\n", path);
-
-
-    // strcpy(pathArr[cnt++], path);
-
-	res = lstat(path, stbuf);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
+struct file* fileList = NULL;
+void add2list(struct file* f)
 {
-	if (strstr(path, mntPath) != path)
-		return 0;
-	strcpy(pathArr[cnt++], path);
-
-	DIR *dp;
-	struct dirent *de;
-
-    printf("--------readdir, %s, buf = %s, offset = %lld \n",
-    		 path, buf, offset);
-
-
-	(void) offset;
-	(void) fi;
-
-	dp = opendir(path);
-	if (dp == NULL)
-		return -errno;
-
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
+	if (fileList == NULL) {
+		fileList = f;
+		return;
 	}
 
-	closedir(dp);
+	struct file* p = fileList;
+	fileList = f;
+	f -> next = p;
+}
+
+static int getattr_callback(const char *path, struct stat *stbuf) {
+	memset(stbuf, 0, sizeof(struct stat));
+
+  	if (strcmp(path, "/") == 0) {
+    	stbuf->st_mode = S_IFDIR | 0755;
+    	stbuf->st_nlink = 2;
+    	return 0;
+  	}
+
+  	struct file* p = fileList;
+  	while (p != NULL) {
+  		if (strcmp(path, p -> filePath) == 0) {
+  			stbuf -> st_mode = S_IFREG | 0777;
+    		stbuf -> st_nlink = 1;
+    		stbuf -> st_size = p -> size;
+    		return 0;
+  		}
+  		p = p -> next;
+  	}
+
+ 	return -ENOENT;
+}
+
+static int readdir_callback(const char *path, void *buf, fuse_fill_dir_t filler,
+    off_t offset, struct fuse_file_info *fi) {
+	(void) offset;
+  	(void) fi;
+
+  	if (strcmp(path, "/") == 0)	{
+		filler(buf, ".", NULL, 0);
+		filler(buf, "..", NULL, 0);
+
+		struct file* p = fileList;
+  		while (p != NULL) {
+  			filler(buf, p -> name, NULL, 0);
+    		p = p -> next;
+  		}
+  	}
+
+  	return 0;
+}
+
+static int open_callback(const char *path, struct fuse_file_info *fi) {
 	return 0;
 }
 
+static int read_callback(const char *path, char *buf, size_t size, off_t offset,
+    struct fuse_file_info *fi) {
 
-static int xmp_unlink(const char *path)
-{
-	int res;
+  // if (strcmp(path, filepath) == 0) {
+  //   size_t len = strlen(filecontent);
+  //   if (offset >= len) {
+  //     return 0;
+  //   }
 
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
+  //   if (offset + size > len) {
+  //     memcpy(buf, filecontent + offset, len - offset);
+  //     return len - offset;
+  //   }
 
-	return 0;
+  //   memcpy(buf, filecontent + offset, size);
+  //   return size;
+  // }
+
+  return -ENOENT;
 }
 
-
-static int xmp_open(const char *path, struct fuse_file_info *fi)
-{
-	int res;
-
-	int i;
-	for (i = 0; i < cnt; ++i)
-		printf("%s\n", pathArr[i]);
-
-	res = open(path, fi->flags);
-	if (res == -1)
-		return -errno;
-
-	close(res);
-	return 0;
-}
-
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
-{
-	int fd;
-	int res;
-
-    printf("--------read, %s, buf = %s, size = %llu, offset = %lld \n",
-    		 path, buf, size, offset);
-
-
-	(void) fi;
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
-}
-
-static int xmp_write(const char *path, const char *buf, size_t size,
-		     off_t offset, struct fuse_file_info *fi)
-{
-	int fd;
-	int res;
-
-    printf("--------write, %s, buf = %s, size = %llu, offset = %lld \n",
-    		 path, buf, size, offset);
-
-	(void) fi;
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pwrite(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
-}
-
-static int xmp_create(const char* path, mode_t mode, struct fuse_file_info* fi) {
-
-    (void) fi;
-
-    printf("--------create, %s\n", path);
-
-    int res;
-    res = creat(path, mode);
-    if(res == -1)
-	return -errno;
-
-    close(res);
-
-    return 0;
-}
-
-
-
-
-static struct fuse_operations xmp_oper = {
-	.getattr	= xmp_getattr,
-	.readdir	= xmp_readdir,
-	.unlink		= xmp_unlink,
-	.open		= xmp_open,
-	.read		= xmp_read,
-	.write		= xmp_write,
-	.create     = xmp_create,
+static struct fuse_operations fuse_example_operations = {
+  .getattr = getattr_callback,
+  .open = open_callback,
+  .read = read_callback,
+  .readdir = readdir_callback,
 };
+
+struct file* fileInit(const char* filename, int size, int isDownload)
+{
+	 struct file* f = malloc (sizeof (struct file));
+	 
+	 strcpy(f -> name, filename);
+	 
+	 char filepath[1000] = "/";
+	 strcat(filepath, filename);
+	 strcpy(f -> filePath, filepath);
+
+	 char bufPath[1000];
+	 strcpy(bufPath, bufBase);
+	 strcat(bufPath, filename);
+	 strcpy(f -> bufPath, bufPath);
+
+	 f -> isDownload = isDownload;
+	 f -> size = size;
+
+	 return f;
+}
+
 
 int main(int argc, char *argv[])
 {
-	pathArr = (char **) malloc(sizeof(char *) * 10000);
-	int i;
-	cnt = 0;
-	for (i = 0; i < 10000; ++i)
-		pathArr[i] = (char *) malloc(sizeof(char) * 1000);
+	struct file* f = fileInit("fileA", 12345, 0);
+	add2list(f);
+	f = fileInit("fileB", 12222, 0);
+	add2list(f);
+	f = fileInit("fileC", 12333, 1);
+	add2list(f);
 
+	struct file* p = fileList;
+	for (p; p != NULL; p = p -> next) {
+		printf("file: %s, filepath = %s, bufpath = %s, size = %d, isDownload = %d\n",
+			p -> name, p -> filePath, p -> bufPath, p -> size, p -> isDownload);
+	}
 
-	// umask(0);
-	return fuse_main(argc, argv, &xmp_oper, NULL);
+ 	return fuse_main(argc, argv, &fuse_example_operations, NULL);
 }
